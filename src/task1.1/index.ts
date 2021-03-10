61602ab47197cfe94843086f97d865a8d1996856
Leonid Petrov <LVPetrov@alfabank.ru>
2021-03-10 23:34:17 +0300

    feat: add task 1.1 solution

---
+++ src/task1.1/index.ts
@@ -0,0 +1,10 @@
+import readline from 'readline';
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on('line', (input) => {
+  console.log(input.split('').reverse().join(''));
+});
