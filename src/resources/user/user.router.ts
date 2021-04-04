9f4a98b58e8e27d59c6266c4aad1863573ad3358
Leonid Petrov <LVPetrov@alfabank.ru>
2021-04-04 12:14:25 +0300

    fix: fix user response

--- src/resources/user/user.router.ts
+++ src/resources/user/user.router.ts
@@ -14,7 +14,7 @@ usersRouter.post('/', validate(userScheme), (req, res, next) => {
   const { password, login, age } = req.body;
   const { id } = userService.createUser({ password, login, age });
   const user = userService.getUserById(id);
-  res.status(200).json(user);
+  res.status(201).json(user);
 });
 
 usersRouter.get('/:id', (req, res, next) => {
@@ -28,7 +28,7 @@ usersRouter.put('/:id', validate(userScheme), (req, res, next) => {
   const { login, age, password } = req.body;
   userService.updateUser({ id, login, age, password });
   const user = userService.getUserById(id);
-  res.status(200).json(user);
+  user ? res.status(200).json(user) : res.status(404).send(`user ${id} not found`);
 });
 
 usersRouter.delete('/:id', (req, res, next) => {
