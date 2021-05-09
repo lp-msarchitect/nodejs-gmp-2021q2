0d4b5110b45b1533d1e9adc0aa4e59c3466acb3a
Leonid Petrov <LVPetrov@alfabank.ru>
2021-05-09 14:48:27 +0300

    feat: add sequalize with cli

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,26 @@
 import express from 'express';
+import db from './db/models';
 import { initModels } from './db/db.client';
 import { usersRouter } from './resources/user';
 import { groupsRouter } from './resources/group';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use('/users', usersRouter);
 app.use('/groups', groupsRouter);
 
+db.sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((error: any) => {
+    console.error('Unable to connect to the database:', error);
+  });
+
 app.listen(port, () => {
-  initModels();
+  // initModels();
   console.log(`Server listening at http://localhost:${port}`);
 });
