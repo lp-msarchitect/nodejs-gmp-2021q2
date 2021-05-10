272ef94a7586485252fa2b2b5947c396eeebcb73
Leonid Petrov <LVPetrov@alfabank.ru>
2021-05-10 03:59:23 +0300

    fix: change table name

--- src/db/migrations/20210510001020-create-user-group.js
+++ src/db/migrations/20210510001020-create-user-group.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('UserGroups', {
+    await queryInterface.createTable('UserGroup', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -25,6 +25,6 @@ module.exports = {
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('UserGroups');
+    await queryInterface.dropTable('UserGroup');
   },
 };
