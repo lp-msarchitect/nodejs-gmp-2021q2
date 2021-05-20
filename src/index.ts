7ff1fc2fb07f08ba700b432d5aa540e0f029802d
Leonid Petrov <LVPetrov@alfabank.ru>
2021-05-20 20:27:34 +0300

    refactor: setup env

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { usersRouter } from './resources/user';
 import { groupsRouter } from './resources/group';
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.SERVER_PORT || 3000;
 
 app.use(express.json());
 app.use('/users', usersRouter);
